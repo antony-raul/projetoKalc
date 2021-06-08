197998781aa158b75778aa1efefe3ff70ae7a21e
antony-raul <raulzin.raul@gmail.com>
2021-06-08 10:33:49 -0300

    entrega 02

--- js/kalcWeb.js
+++ js/kalcWeb.js
@@ -1,19 +1,54 @@
+// teclas 
+const keys = document.querySelectorAll(".buttons .numbers button:not(#clear)")
+const clear = document.querySelector("#clear")
+
+const ops = document.querySelectorAll(".buttons .operators button")
 
 
+//visor
+const screen = document.querySelector(".screen")
 
 
+// eventos teclas
 
-function printText(id, text){
-    document.getElementById(id).onclick = function (){
-        console.log(text)
+keys.forEach(function (key){
+    key.addEventListener("click", function (){
+         const keyValue = key.getAttribute("data-val")
+         insertValue(keyValue)
+    })
+})
+
+// eventos operações
+
+ops.forEach(function (op){
+    op.addEventListener("click", function (){
+        if (op.innerText == "="){
+            showResult(screen.innerText)
+        }else{
+            const opValue = op.getAttribute("data-val")
+        insertValue(opValue)
+        }
+        
+    })
+})
+
+clear.addEventListener("click", function (){
+    screen.innerText = "0"
+})
+
+//inserir operação
+const insertValue = function (value){
+    if (screen.innerText.length > 13) {
+        return
+    }else if(screen.innerText == 0){
+        screen.innerText = ""
     }
+        screen.innerText = screen.innerText + value
+    
 }
 
-let btn = document.getElementsByTagName('button')
+//exe operação
 
-for (let i = 0; i < btn.length; i++) {
-    let id = btn[i].id
-    let text = btn[i].innerText
-
-    printText(id,text)
+const showResult= function (operation){
+    screen.innerText = eval(operation)
 }
